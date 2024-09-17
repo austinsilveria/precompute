80df8a89e367fe5d5c1da90cfa2893450aff40a7
Austin Silveria <austinsilveria@gmail.com>
2024-09-16 18:55:41 -0700

    add seq len and noise comparisons

--- research/optspectrum/run.py
+++ research/optspectrum/run.py
@@ -1,5 +1,9 @@
 import torch
 
+from datasets import load_dataset
+from transformers import AutoTokenizer
+from diffusers import DDPMScheduler
+
 from precompute import (
     Hook,
     HookVariableNames,
@@ -11,7 +15,29 @@ from precompute import (
 model_name = 'facebook/opt-125m'
 model = HookedOPTForCausalLM.from_pretrained(model_name, torch_dtype=torch.float16).to('cuda')
 
-inputs = torch.load('opt-30b-c4-inputs.pt').to('cuda')
+raw_datasets = load_dataset('c4', 'en', streaming=True)
+context_length = 512
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+def tokenize(element):
+    outputs = tokenizer(
+        element["text"],
+        truncation=True,
+        max_length=context_length,
+        return_overflowing_tokens=True,
+        return_length=True,
+    )
+    input_batch = []
+    for length, input_ids in zip(outputs["length"], outputs["input_ids"]):
+        if length == context_length:
+            input_batch.append(input_ids)
+    return {"input_ids": input_batch}
+
+tokenized_datasets = raw_datasets.map(
+    tokenize, batched=True, remove_columns=raw_datasets["train"].column_names
+)
+
+inputs = torch.stack([torch.tensor(i['input_ids']) for i in tokenized_datasets["validation"].take(8)]).to('cuda')
 
 pctx = PrecomputeContext(model.config)
 
@@ -39,11 +65,17 @@ def log_post_mlp(x, ctx):
     ctx.context['post-mlp'][ctx.context['layer']] = x.cpu()
     return x
 
+# [b, n, d]
+def log_post_final_layer_norm(x, ctx):
+    ctx.context['post-final-layer-norm'] = x.cpu()
+    return x
+
 hooks = [
     Hook(HookVariableNames.POST_TOK_EMBEDDINGS, log_post_tok_embeddings),
     Hook(HookVariableNames.POST_POS_EMBEDDINGS, log_post_pos_embeddings),
     Hook(HookVariableNames.MLP_RESIDUAL, log_mlp_residual),
     Hook(HookVariableNames.POST_MLP, log_post_mlp),
+    Hook(HookVariableNames.POST_FINAL_LAYER_NORM, log_post_final_layer_norm),
 ]
 pctx.add_hooks(hooks)
 
@@ -75,7 +107,7 @@ hidden_states_freqs, hidden_states_mps = compute_power_spectrum(hidden_states)
 mlp_residuals_freqs, mlp_residuals_mps = compute_power_spectrum(pctx.context['mlp-residual'][:, :, 1:])
 
 artifact_metadata = {
-    'name': 'optspectrum-125m-hidden-states',
+    'name': 'optspectrum-125m-hidden-states' + '-seq-len-' + str(context_length),
     'visualization': 'line',
     'y_name': 'Power',
     'description': 'Power spectrum along sequence dimension of hidden states for OPT-125M model, averaged across batch and latent dimensions.',
@@ -91,7 +123,7 @@ write_artifact(artifact_metadata, columns)
 
 
 artifact_metadata = {
-    'name': 'optspectrum-125m-mlp-residuals',
+    'name': 'optspectrum-125m-mlp-residuals' + '-seq-len-' + str(context_length),
     'visualization': 'line',
     'y_name': 'Power',
     'description': 'Power spectrum along sequence dimension of MLP residuals for OPT-125M model, averaged across batch and latent dimensions.',
@@ -103,6 +135,41 @@ for i in range(mlp_residuals_mps.shape[0]):
 
 write_artifact(artifact_metadata, columns)
 
+
+# Forward diffusion
+final_hidden_states = (pctx.context['post-final-layer-norm'][:, 1:, :]).float()
+num_steps = 1000
+beta_end = 0.02
+
+noise_scheduler = DDPMScheduler(num_train_timesteps=num_steps, beta_schedule='linear', beta_end=beta_end)
+timesteps = torch.tensor([500], device=hidden_states.device).repeat(final_hidden_states.shape[0])
+
+noise = torch.randn_like(final_hidden_states, device=hidden_states.device)
+scheduled_noisy_hidden_states = noise_scheduler.add_noise(final_hidden_states, noise, timesteps)
+timesteps = torch.tensor([250], device=hidden_states.device).repeat(final_hidden_states.shape[0])
+quarter_noise = noise_scheduler.add_noise(final_hidden_states, noise, timesteps)
+added_noise = final_hidden_states + noise
+
+hidden_states_freqs, hidden_states_mps = compute_power_spectrum(final_hidden_states.unsqueeze(0))
+noisy_hidden_states_freqs, noisy_hidden_states_mps = compute_power_spectrum(scheduled_noisy_hidden_states.unsqueeze(0))
+_, qhs = compute_power_spectrum(quarter_noise.unsqueeze(0))
+added_noise_hidden_states_freqs, added_noise_hidden_states_mps = compute_power_spectrum(added_noise.unsqueeze(0))
+noise_hidden_states_freqs, noise_hidden_states_mps = compute_power_spectrum(noise.unsqueeze(0))
+
+artifact_metadata = {
+    'name': 'optspectrum-125m-final-states-noise-comparison' + '-seq-len-' + str(context_length),
+    'visualization': 'line',
+    'y_name': 'Power',
+    'description': 'Power spectrum along sequence dimension of final hidden states for an untrained OPT-125M model, averaged across batch and latent dimensions. Compared against quarter/half noise from DDPM scheduler, directly added noise, and pure noise.',
+    'config': model.config.to_dict(),
+}
+columns = {'Frequency': hidden_states_freqs.numpy()}
+columns['Final Hidden States'] = hidden_states_mps[0].numpy()
+columns['Quarter Noise'] = qhs[0].numpy()
+columns['Half Noise'] = noisy_hidden_states_mps[0].numpy()
+columns['Added Noise'] = added_noise_hidden_states_mps[0].numpy()
+columns['Noise'] = noise_hidden_states_mps[0].numpy()
+write_artifact(artifact_metadata, columns)
 
 # Untrained
 config = model.config
@@ -123,7 +190,7 @@ hidden_states_freqs, hidden_states_mps = compute_power_spectrum(hidden_states)
 mlp_residuals_freqs, mlp_residuals_mps = compute_power_spectrum(pctx.context['mlp-residual'][:, :, 1:])
 
 artifact_metadata = {
-    'name': 'optspectrum-untrained-125m-hidden-states',
+    'name': 'optspectrum-untrained-125m-hidden-states' + '-seq-len-' + str(context_length),
     'visualization': 'line',
     'y_name': 'Power',
     'description': 'Power spectrum along sequence dimension of hidden states for an untrained OPT-125M model, averaged across batch and latent dimensions.',
@@ -139,7 +206,7 @@ write_artifact(artifact_metadata, columns)
 
 
 artifact_metadata = {
-    'name': 'optspectrum-untrained-125m-mlp-residuals',
+    'name': 'optspectrum-untrained-125m-mlp-residuals' + '-seq-len-' + str(context_length),
     'visualization': 'line',
     'y_name': 'Power',
     'description': 'Power spectrum along sequence dimension of MLP residuals for an untrained OPT-125M model, averaged across batch and latent dimensions.',
